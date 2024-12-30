e5872d1e005c2c70cd702dfb4d4096cc8d075e20
Rahulravib <rahul.ravi@bootlabstech.com>
2024-12-30 14:37:38 +0530

    Updated Subnets and Routing table and Configured Backend to S3

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "vpc1" {
 
 #Attach the internet gateway to the VPC
 resource "aws_internet_gateway" "vpc1-igw" {
-  vpc_id = aws_vpc.vpc1.id
+  vpc_id = aws_vpc.vpc1.id #implicit dependency on the VPC resource
 
   tags = {
     Name = "vpc1-igw"
