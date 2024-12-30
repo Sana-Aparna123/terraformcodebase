2a9f6c651bca3ae1890ffcffa1f5f48378c349b5
Rahulravib <rahul.ravi@bootlabstech.com>
2024-12-30 16:32:28 +0530

    Adding security group in terraform code

---
+++ security.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.vpc1.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+  ingress {
+    description = "Allow All Traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
