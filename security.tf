1adb5f55aa6b310f5cf26222278b423acdcc4ba9
Rahulravib <rahul.ravi@bootlabstech.com>
2024-12-31 11:50:44 +0530

    changing protocol in security.tf file

--- security.tf
+++ security.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "allow_tls" {
     description = "Allow_tcp22_traffic"
     from_port   = 22
     to_port     = 22
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
